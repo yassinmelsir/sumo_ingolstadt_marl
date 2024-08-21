8386db6e35b363e0b6f462d603b82a573caaaef8
Yassin Elsir <elsiryas@hu-berlin.de>
2024-08-21 17:35:11 +0200

    added multi-platform support

--- containers/network_info/docker/build.sh
+++ containers/network_info/docker/build.sh
@@ -1,7 +1,8 @@
-DOCKERFILE_PATH="/Users/yme/York/IRP/sumo_ingolstadt_marl/containers/network_info/Dockerfile"
+DOCKERFILE_PATH="/Users/yme/York/IRP/sumo_ingolstadt_marl/containers/network_info/docker/Dockerfile"
 IMAGE_NAME="yassinmelsir/simulation-marl-ni:latest"
 ROOT_DIR="/Users/yme/York/IRP/sumo_ingolstadt_marl/"
 
 docker build -t $IMAGE_NAME \
   -f $DOCKERFILE_PATH \
+  --platform linux/amd64,linux/arm64 \
   $ROOT_DIR
