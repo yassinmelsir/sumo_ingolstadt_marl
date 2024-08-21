b393fba0011dfcf1decffeac0a588757d2415604
Yassin Elsir <elsiryas@hu-berlin.de>
2024-08-21 17:42:13 +0200

    multi-platform created

--- containers/network_info/docker/build.sh
+++ containers/network_info/docker/build.sh
@@ -2,7 +2,7 @@ DOCKERFILE_PATH="/Users/yme/York/IRP/sumo_ingolstadt_marl/containers/network_inf
 IMAGE_NAME="yassinmelsir/simulation-marl-ni:latest"
 ROOT_DIR="/Users/yme/York/IRP/sumo_ingolstadt_marl/"
 
-docker build -t $IMAGE_NAME \
+docker buildx build -t $IMAGE_NAME \
   -f $DOCKERFILE_PATH \
   --platform linux/amd64,linux/arm64 \
   $ROOT_DIR
