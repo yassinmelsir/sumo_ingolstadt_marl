f0bdfd583bd198a82e8ce5b709e48d1d62789897
YM Elsir <pdm523@login1.viking2.yor.alces.network>
2024-08-22 22:28:45 +0100

    updated jobscripts

--- containers/network_info/apptainer/js.sh
+++ containers/network_info/apptainer/js.sh
@@ -1,5 +1,6 @@
-SIM_NAME="network_info/2023-06-19_01"
-CONTAINER_CONFIG_FILE_PATH="/usr/app/simulation/Ingolstadt_SUMO_365/2023-06-19.sumocfg"
+SIM_NAME=${SIM_NAME}
+CONTAINER_CONFIG_FILE_PATH=${CONTAINER_CONFIG_FILE_PATH}
+CORES=$CORES
 
 LOCAL_DATA_FILE_PATH="/users/pdm523/sumo_ingolstadt_marl/data"
 LOCAL_SIMULATION_FILE_PATH="/users/pdm523/sumo_ingolstadt_marl/simulation"
@@ -8,7 +9,7 @@ IMAGE_PATH="/users/pdm523/sumo_ingolstadt_marl/containers/images/simulation-marl
 CONTAINER_DATA_FILE_PATH="/usr/app/data"
 CONTAINER_SIMULATION_FILE_PATH="/usr/app/simulation"
 
-SUMO_CMD="/usr/bin/sumo -c $CONTAINER_CONFIG_FILE_PATH --threads 4 --device.rerouting.threads 4 --verbose true"
+SUMO_CMD="/usr/bin/sumo -c $CONTAINER_CONFIG_FILE_PATH --threads $CORES --device.rerouting.threads $CORES --verbose true"
 
 RUN_SCRIPT="python3 /usr/app/src/procedures/network_info/main.py \
                       --sumo_cmd \"${SUMO_CMD}\" \
