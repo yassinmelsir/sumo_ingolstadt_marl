bfd862cd5c4fb6a6434918f480847bda59ac9897
YM Elsir <pdm523@login2.viking2.yor.alces.network>
2024-08-22 17:48:16 +0100

    some container file updates

--- containers/network_info/apptainer/build.sh
+++ containers/network_info/apptainer/build.sh
@@ -1,4 +1,6 @@
 IMAGE_NAME="yassinmelsir/simulation-marl-ni:amd64"
 IMAGE_PATH="/users/pdm523/sumo_ingolstadt_marl/containers/images/simulation-marl-ni.sif"
 
+module load Apptainer/latest
+
 apptainer build $IMAGE_PATH "docker://$IMAGE_NAME"
--- containers/network_info/apptainer/run.sh
+++ containers/network_info/apptainer/run.sh
@@ -1,21 +1,25 @@
-SIM_NAME="network_info/2023-06-19"
-LOCAL_DATA_FILE_PATH="/user/pdm523/sumo_ingolstadt_marl/data"
-LOCAL_SIMULATION_FILE_PATH="/user/pdm523/sumo_ingolstadt_marl/simulation"
-IMAGE_NAME="yassinmelsir/simulation-marl-ni:amd64"
+SIM_NAME="network_info/2023-06-19:01"
+LOCAL_DATA_FILE_PATH="/users/pdm523/sumo_ingolstadt_marl/data"
+LOCAL_SIMULATION_FILE_PATH="/users/pdm523/sumo_ingolstadt_marl/simulation"
+IMAGE_PATH="/users/pdm523/sumo_ingolstadt_marl/containers/images/simulation-marl-ni.sif"
 
-SUMO_CMD="/usr/bin/sumo -c $CONTAINER_CONFIG_FILE_PATH --threads 2 --device.rerouting.threads 1 --verbose true --end 1000"
+CONTAINER_CONFIG_FILE_PATH="/usr/app/simulation/Ingolstadt_SUMO_365/2023-06-19.sumocfg"
+CONTAINER_DATA_FILE_PATH="/usr/app/data/"
+CONTAINER_SIMULATION_FILE_PATH="/usr/app/simulation/"
+
+SUMO_CMD="/usr/bin/sumo -c $CONTAINER_CONFIG_FILE_PATH --threads 4 --device.rerouting.threads 4 --verbose true"
 
 RUN_SCRIPT="python3 /usr/app/src/procedures/network_info/main.py \
                       --sumo_cmd \"${SUMO_CMD}\" \
                       --data_file_path ${CONTAINER_DATA_FILE_PATH}"
 
-CONTAINER_CONFIG_FILE_PATH="/usr/app/simulation/Ingolstadt SUMO 365/2023-06-19.sumocfg"
-CONTAINER_DATA_FILE_PATH="/usr/app/data/"
-CONTAINER_SIMULATION_FILE_PATH="/usr/app/simulation/"
+
 
 SIMULATION_MOUNT_PATH="$LOCAL_SIMULATION_FILE_PATH:$CONTAINER_SIMULATION_FILE_PATH"
 DATA_MOUNT_PATH="$LOCAL_DATA_FILE_PATH/$SIM_NAME:$CONTAINER_DATA_FILE_PATH"
 
-apptainer exec --bind $SIMULATION_MOUNT_PATH \
-               --bind $DATA_MOUNT_PATH \
-               $IMAGE_NAME sh -c "$RUN_SCRIPT"
+module load Apptainer/latest
+
+apptainer exec --bind "$SIMULATION_MOUNT_PATH" \
+               --bind "$DATA_MOUNT_PATH" \
+               $IMAGE_PATH sh -c "$RUN_SCRIPT"
